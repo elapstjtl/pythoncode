1f7954ddaeb83592303b2991e6ffb8d97336b1c8
elapstjtl <elapstjtl1@163.com>
2020-11-28 22:05:27 +0800

    因为环境被污染，改在pycharm上编写

--- python/function/q_sort.py
+++ python/function/q_sort.py
@@ -1,5 +1,9 @@
-
-a=input()
-print(a)
-b = a*2
-print(b)
+'''
+Author: your name
+Date: 2020-11-27 18:42:56
+LastEditTime: 2020-11-28 21:45:10
+LastEditors: Please set LastEditors
+Description: In User Settings print
+FilePath: \pythoncode\python\function\q_sort.py
+'''
+print("hello world")
