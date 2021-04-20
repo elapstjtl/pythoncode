2d4f446b8f0f8ed3d4c7c9c163248e0bfd630e46
elapstjtl <elapstjtl1@163.com>
2021-04-20 10:03:02 +0800

    cvxpy解决整数规划

--- code/tool/LP.py
+++ code/tool/LP.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-04-18 12:32:03
-LastEditTime: 2021-04-18 12:35:35
+LastEditTime: 2021-04-18 14:22:41
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \pythoncode\code\tool\LP.py
@@ -9,10 +9,11 @@ FilePath: \pythoncode\code\tool\LP.py
 from scipy import optimize
 import numpy as np
 
-c = np.array([])
-A = np.array([])
-b = np.array([])
-Aeq = np.array([])
-beq = np.array([])
-res = optimize.linprog(c,A,b,Aeq,beq)
-print(res)
+c = np.array([])#目标函数
+A = np.array([[])#不等式左式，二维数组
+b = np.array([])#不等式右式，一维
+Aeq = np.array([[]])#等式左式，二维数组
+beq = np.array([])#等式右式，一维
+
+res = optimize.linprog(-c,A,b,Aeq,beq)#解
+print(res) 
