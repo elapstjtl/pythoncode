cb6d0fef1550626e5e63469a5d1001c7761c668c
elapstjtl <elapstjtl1@163.com>
2021-05-04 15:38:00 +0800

    1

---
+++ code/tool/Simulated_annealing_algorithm.py
@@ -0,0 +1,9 @@
+'''
+Author: your name
+Date: 2021-05-04 15:23:06
+LastEditTime: 2021-05-04 15:23:07
+LastEditors: Please set LastEditors
+Description: 退火算法模板
+FilePath: \pythoncode\code\tool\Simulated_annealing_algorithm.py
+'''
+def 
--- math/homework/long_xia_yang_zhi/1.py
+++ math/homework/long_xia_yang_zhi/1.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-05-03 16:00:03
-LastEditTime: 2021-05-03 18:51:02
+LastEditTime: 2021-05-04 15:08:39
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \pythoncode\math\homework\long_xia_yang_zhi\1.py
@@ -31,15 +31,18 @@ longnum = [3.8,3.93,3.96,4.03,4.11,4.14,4.17,4.18,4.27,4.4,4.59,4.84,4.99,5.08,5
 x = np.array(year) 
 y = np.array(longnum)
 
-z1 = np.polyfit(x,y,10) 
+z1 = np.polyfit(x,y,4) 
 p1= np.poly1d(z1)
 print(p1) #输出拟合的方程
 yvals = p1(x)
+ansx = list(range(21,31,1))
+for i in ansx:
+    print(p1(i))
 
-plot1 = plt.plot(x,y,'*',label='原数据')
-plot2 = plt.plot(x,yvals,'r',label='拟合曲线')
-plt.xlabel('年份')
-plt.ylabel('龙虾只数/千只')
-plt.legend(loc=4)  
-plt.title('小龙虾只数关于时间拟合曲线')
-plt.show()
+# plot1 = plt.plot(x,y,'*',label='原数据')
+# plot2 = plt.plot(x,yvals,'r',label='拟合曲线')
+# plt.xlabel('年份')
+# plt.ylabel('龙虾只数/千只')
+# plt.legend(loc=4)  
+# plt.title('小龙虾只数关于时间拟合曲线')
+# plt.show()
